11f2682a4f5a82c05c89db09f528ef44a3a1d5ae
Yadavji885 <123755614+Yadavji885@users.noreply.github.com>
2023-04-04 12:19:18 +0530

    Add files via upload

---
+++ %Mine%/dictionary and sets/practice 02.py
@@ -0,0 +1,12 @@
+a1 = int(input("Enter a no. :\n"))
+a2 = int(input("Enter a no. :\n"))
+a3 = int(input("Enter a no. :\n"))
+a4 = int(input("Enter a no. :\n"))
+a5 = int(input("Enter a no. :\n"))
+a6 = int(input("Enter a no. :\n"))
+a7 = int(input("Enter a no. :\n"))
+a8 = int(input("Enter a no. :\n"))
+
+s = {a1,a2,a3,a4,a5,a6,a7,a8}
+
+print(s)
---
+++ %Mine%/dictionary and sets/practice 03.py
@@ -0,0 +1,13 @@
+a1 = input("Enter the favourate language of Billi Bhatia : \n")
+a2 = input("Enter the favourate language of Helicopter : \n")
+a3= input("Enter the favourate language of Takla : \n")
+a4 = input("Enter the favourate language of Raju Chai Wala : \n")
+
+mydict = {
+    "Billi Bhatia" : a1,
+    "Helicopter" : a2,
+    "Takla" : a3,
+    "Raju Chai Wala" : a4
+}
+
+print (mydict)
