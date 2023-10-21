21452897bd0ae3fde575301bc64e96e65d298b5f
Yadavji885 <123755614+Yadavji885@users.noreply.github.com>
2023-10-21 17:30:18 +0530

    jero kanta.py

--- %Mine%/my projects/jero kanta.py
+++ %Mine%/my projects/jero kanta.py
@@ -1,11 +1,11 @@
 jk = "__1__|__2__|__3__\n__4__|__5__|__6__\n  7  |  8  |  9"
-
+t = jk
 print(jk)
 
 player1 = input("Enter First player's Name (You will get X) :\n")
 player2 = input("Enter second player's Name (You will get O) :\n")
 
-for i in range(5):
+for i in range(6):
     print(f"{player1}'s turn")
     x = input("Enter any number which you want to replace :\n")
     jk = jk.replace(x,"X")
@@ -64,5 +64,6 @@ for i in range(5):
     if jk[14]=="O" and jk[26]=="O" and jk[38]=="O":
         print(f"{player1} won!!")
         break
-    else:
+    if jk[2] != t[2] and jk[8] != t[8] and jk[14] != t[14] and jk[20] != t[20] and jk[26] != t[26] and jk[32] != t[32] and jk[38] != t[38] and jk[44] != t[44] and jk[50] != t[50]:
         print("It's a tie match")
+        break
